99ecb863a8e48d02f66dff37bc86f650d6f0af85
Alex Berke <berke.alexandra@gmail.com>
2014-05-09 17:09:30 -0400

    mongo setup for heroku

--- config.py
+++ config.py
@@ -3,12 +3,33 @@ import os
 HOST = os.getenv('HOST', '127.0.0.1')
 PORT = os.getenv('PORT', 3000)
 
+# - MONGO ----------------------------------
+
+# set Development defaults
 MONGO_DBNAME 		= 'OPP'
-MONGODB_HOST 		= os.environ.get("DOTCLOUD_API_DB_MONGODB_HOST", "localhost")
-MONGODB_PORT 		= os.environ.get("DOTCLOUD_API_DB_MONGODB_PORT", "27017")
-MONGODB_USERNAME 	= os.environ.get("API_DB_MONGODB_LOGIN", "")
-MONGODB_PASSWORD 	= os.environ.get("API_DB_MONGODB_PASSWORD", "")
+MONGODB_HOST 		= "localhost"
+MONGODB_PORT 		= "27017"
 MONGODB_DB 			= "OPP_database"
+
+
+HEROKU_MONGODB_URL 	= os.environ.get("MONGOHQ_URL", None)
+if HEROKU_MONGODB_URL:
+	print('HEROKU_MONGODB_URL', HEROKU_MONGODB_URL)
+	MONGO_DBNAME 	= "app24775728"
+	MONGODB_HOST 	= "oceanic.mongohq.com"
+	MONGODB_PORT 	= "10012"
+	MONGODB_USERNAME= os.environ.get("heroku")
+	MONGODB_PASSWORD= HEROKU_MONGODB_URL.split(':')[2].split('@')[0]
+
+
+# MONGO_DBNAME 		= 'OPP'
+# MONGODB_HOST 		= os.environ.get("DOTCLOUD_API_DB_MONGODB_HOST", "localhost")
+# MONGODB_PORT 		= os.environ.get("DOTCLOUD_API_DB_MONGODB_PORT", "27017")
+# MONGODB_USERNAME 	= os.environ.get("API_DB_MONGODB_LOGIN", "")
+# MONGODB_PASSWORD 	= os.environ.get("API_DB_MONGODB_PASSWORD", "")
+# MONGODB_DB 			= "OPP_database"
+
+# ---------------------------------- MONGO -
 
 DEBUG = os.getenv('TESTING', True)
 
