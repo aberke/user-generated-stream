cd492070b7129dafd7175b1b588ee85384c3fc15
Alex Berke <berke.alexandra@gmail.com>
2014-05-12 10:26:11 -0400

    cleanup after debugging heroku mongodb connection

--- config.py
+++ config.py
@@ -4,34 +4,30 @@ from urlparse import urlparse
 HOST = os.getenv('HOST', '127.0.0.1')
 PORT = os.getenv('PORT', 3000)
 
+
 # - MONGO ----------------------------------
 
 # set Development defaults
-#MONGO_DBNAME 		= "OPP"
 MONGODB_HOST 		= "localhost"
 MONGODB_PORT 		= "27017"
 MONGODB_DB 			= "OPP"
+MONGODB_USERNAME	= ""
+MONGODB_PASSWORD	= ""
+
 
 # if on Heroku - set heroku variables
 HEROKU_MONGODB_URL 	= os.environ.get("MONGOHQ_URL", None)
 if HEROKU_MONGODB_URL:
 	db_info 		= urlparse(HEROKU_MONGODB_URL)
 
-	#MONGO_DBNAME 	= db_info.path.replace('/', '')
 	MONGODB_DB		= db_info.path.replace('/', '')
 	MONGODB_HOST 	= db_info.hostname
 	MONGODB_PORT 	= db_info.port
 	MONGODB_USERNAME= db_info.username
 	MONGODB_PASSWORD= db_info.password
 
-print('---------------')
-print('MONGODB_HOST',MONGODB_HOST)
-print('MONGODB_PORT', MONGODB_PORT)
-print('MONGODB_USERNAME',MONGODB_USERNAME)
-print('MONGODB_PASSWORD', MONGODB_PASSWORD)
-print('MONGODB_DB',MONGODB_DB)
-
 # ---------------------------------- MONGO -
+
 
 DEBUG = os.getenv('TESTING', True)
 
